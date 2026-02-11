b0e6e19a48754f43600a7505d4c7091b1b0b367f
2sayle <132142945+2sayle@users.noreply.github.com>
2026-02-11 21:20:45 +0100

    Ajout des fichiers wifi_handler.c et wifi_handler.h pour la gestion du Wi-Fi

---
+++ drivers/wifi_sta/wifi_handler.c
@@ -0,0 +1,5 @@
+//
+// Created by Elyass Jaoudat on 26/01/2026.
+//
+
+#include "wifi_handler.h"
---
+++ drivers/wifi_sta/wifi_handler.h
@@ -0,0 +1,8 @@
+//
+// Created by Elyass Jaoudat on 26/01/2026.
+//
+
+#ifndef LAERA_FW_WIFI_HANDLER_H
+#define LAERA_FW_WIFI_HANDLER_H
+
+#endif //LAERA_FW_WIFI_HANDLER_H
